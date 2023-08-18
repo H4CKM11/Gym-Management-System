c6ad7de1a6014a9560c479492e945db05743e848
H4CKM11 <eduardomartinez73119@gmail.com>
2023-08-17 21:54:23 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  theme: {
+    extend: {colors: {
+      'Golds-Gym-Yellow' : '#ffed00'
+    },
+    screens: {
+      '1920res' : '1920px',
+      '2500res' : '2500px'
+    }
+  },
+  },
+  plugins: [],
+}
+
